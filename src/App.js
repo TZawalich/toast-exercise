fcd214073142c8b4959eacf2a08eecc16151d6d0
TZawalich <t.zawalich@gmail.com>
2022-05-05 14:30:16 -0700

    set onMessage callback function for formSubmission

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@mui/material/Container';
+import { onMessage, saveLikedFormSubmission } from './service/mockServer';
 
 import Header from './Header';
 import Content from './Content';
 
 function App() {
+const [toastData, setToastData] = useState([]);
+
 // current toast state, all toast state?, liked state
 
 //set callback onMessage useeffect
+
+useEffect(() => {
+  const toastCallback = (formSubmission) => {
+    console.log(formSubmission)
+    setToastData((prevState) => [...prevState, formSubmission])
+  }
+
+  onMessage(toastCallback);
+}, [])
+
+console.log("state", toastData)
 
 // snackbar logic if not in own component
 
