7e24c8f88194fb730409bc6c0213e3ff8145d93c
TZawalich <t.zawalich@gmail.com>
2022-05-05 14:25:12 -0700

    outline notes on page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import Header from './Header';
 import Content from './Content';
 
 function App() {
+// current toast state, all toast state?, liked state
+
+//set callback onMessage useeffect
+
+// snackbar logic if not in own component
+
+//like handler/ data
+//save liked toast to "db" and loop if fails
+
   return (
     <>
       <Header />
       <Container>
         <Content />
       </Container>
+      {/* toast notification snackbar */}
     </>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -3,12 +3,30 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 export default function Content() {
+
+//likedList
+//liked State?
+
+//fetch loading state 
+//loop if fail
+
+//empty liked list msg?
+
+//fetch list logic
+
+//push new liked toast to page
+//push list to react version, let handler in app deal with saving
+
+
+
+
   return (
     <Box sx={{marginTop: 3}}>
       <Typography variant="h4">Liked Form Submissions</Typography>
 
       <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
-        TODO: List of liked submissions here (delete this line)
+        {/* List items map */}
+        {/* if no list items, empty liked list msg here */}
       </Typography>
     </Box>
   );
